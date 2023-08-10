b5a033208be2f4800f87cdbaba3e3e6908c5cc45
aesperer <piyrw9754@gmail.com>
2023-08-11 00:29:25 +0900

    connect database local

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
-package gorest
+package main
+
+import "gorest/database"
+
+func main() {
+	config := database.Config{
+		ServerName: "localhost:3306",
+		User:       "root",
+		Password:   "Esperer123!",
+		DB:         "gorest",
+	}
+
+	connectionString := database.GetConnectionString(config)
+	err := database.Connect(connectionString)
+
+	if err != nil {
+		panic(err.Error())
+	}
+}
